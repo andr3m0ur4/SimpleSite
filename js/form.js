8a48da6deab1000200ca5b405f632863527557cd
André Moura <andre.benedicto@etec.sp.gov.br>
2020-08-29 00:42:30 -0300

    Melhorando formulário de contato

--- js/form.js
+++ js/form.js
@@ -1,9 +1,24 @@
 $(() => {
 
-    $('form').ajaxForm({
-        success: () => {
-            alert('Formulário enviado com sucesso!')
-        }
+    $('form').submit(e => {
+        $(e.target).ajaxSubmit({
+            beforeSubmit: () => {
+                $(e.target).find('button').attr('disabled', 'true')
+                $(e.target).find('button').animate({ 'opacity': '0.4' })
+                $(e.target).find('button').html('Enviando')
+            },
+            success: () => {
+                // Aqui você pode inserir uma idv, por exemplo.
+                // Qualquer mensagem de sucesso, após o formulário ter sido enviado.
+                alert('Formulário foi enviado com sucesso!')
+                $(e.target).find('button').removeAttr('disabled')
+                $(e.target).find('button').animate({ 'opacity': '1' })
+                $(e.target).find('button').html('Enviar')
+                $(e.target)[0].reset()
+            }
+        })
+
+        e.preventDefault()
     })
 
 })
